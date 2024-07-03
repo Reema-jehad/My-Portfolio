0d55b5f00f2e462ff30cf1b8c810ec69dab26859
Reema <reemajehad0@gmail.com>
2024-07-03 03:27:39 +0300

    any

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable react/prop-types */
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { github,live,live2 } from "../assets";
+import { github, live } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 import { Tilt } from "react-tilt";
+import PropTypes from 'prop-types';
 
 const ProjectCard = ({
   index,
@@ -14,61 +16,49 @@ const ProjectCard = ({
   image,
   source_code_link,
   live_site,
-}) => {
-  return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-      >
-        <div className="relative w-full h-[230px]">
-          <img
-            src={image}
-            alt={name}
-            className="w-full h-full object-cover rounded-2xl"
-          />
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover gap-2">
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img
-                src={github}
-                alt="github"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
-            <div
-              onClick={() => window.open(live_site, "_blank")}
-              className=" bg-white w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img
-                src={live}
-                alt="github"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
+}) => (
+  <motion.div variants={fadeIn("up", "spring")}>
+    <Tilt className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full">
+      <div className="relative w-full h-[230px]">
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-full object-cover rounded-2xl" />
+        <div className="absolute inset-0 flex justify-end m-3 card-img_hover gap-2">
+          <div
+            onClick={() => window.open(source_code_link, "_blank")}
+            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+          >
+            <img
+              src={github}
+              alt="github"
+              className="w-1/2 h-1/2 object-contain" />
+          </div>
+          <div
+            onClick={() => window.open(live_site, "_blank")}
+            className=" bg-white w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+          >
+            <img
+              src={live}
+              alt="github"
+              className="w-1/2 h-1/2 object-contain" />
           </div>
         </div>
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
-              #{tag.name}
-            </p>
-          ))}
-        </div>
-      </Tilt>
-    </motion.div>
-  );
-};
+      </div>
+      <div className="mt-5">
+        <h3 className="text-white font-bold text-[24px]">{name}</h3>
+        <p className="mt-2 text-secondary text-[14px]">{description}</p>
+      </div>
+      <div className="mt-4 flex flex-wrap gap-2">
+        {tags.map((tag) => (
+          <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+            #{tag.name}
+          </p>
+        ))}
+      </div>
+    </Tilt>
+  </motion.div>
+);
 
 const Works = () => {
   return (
@@ -82,7 +72,7 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
-          Below are some of the key projects I've completed, showcasing my
+          Below are some of the key projects I have completed, showcasing my
           ability to use a wide range of technologies including HTML, CSS,
           JavaScript, React, Bootstrap, Tailwind, Sass, and TypeScript.
         </motion.p>
